b893d55b6c9183d5e47648340294d87138333074
Nataliia Kriuchkova <natalyshimko@gmail.com>
2026-03-08 14:56:03 +0100

    Add framework configuration

--- src/test/java/com/player/config/Config.java
+++ src/test/java/com/player/config/Config.java
@@ -1,4 +1,25 @@
 package com.player.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Config {
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input =
+                     Config.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in classpath");
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String getAppBaseUrl() {
+        return properties.getProperty("app.base.url");
+    }
 }
--- src/test/java/com/player/core/RestAssuredConfig.java
+++ src/test/java/com/player/core/RestAssuredConfig.java
@@ -1,4 +1,10 @@
 package com.player.core;
 
+import com.player.config.Config;
+import io.restassured.RestAssured;
+
 public class RestAssuredConfig {
+    static {
+        RestAssured.baseURI = Config.getAppBaseUrl();
+    }
 }
